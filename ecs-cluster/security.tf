66af3e9841b92bd7fbb15641a3aa25b486a0bce0
Aleksandr Titov <t.sander.v@gmail.com>
2020-08-02 10:19:00 +0300

    Update of the Security Groups

--- ecs-cluster/security.tf
+++ ecs-cluster/security.tf
@@ -6,37 +6,54 @@ data "aws_vpc" "main" {
 
 
 #
-# Security group for ECS instances
-resource "aws_security_group" "ecs_security_group" {
-  name        = format("%s-%s", var.ecs_cluster_name, "security-group")
+# Security group for ECS EC2 instances
+resource "aws_security_group" "ec2_security_group" {
+  count = var.capacity_provider_ec2 == "true" ? 1 : 0
+
+  name        = format("%s-%s", var.ecs_cluster_name, "ecs-ec2-sg")
   vpc_id      = data.aws_vpc.main.id
-  description = "ECS EC2 Security Group"
+  description = format("ECS %s EC2 Security Group", var.ecs_cluster_name)
+
+  ingress {
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+  }
+  ingress {
+    security_groups = [aws_security_group.alb_security_group.id]
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#
+# Security group for ECS Application Load Balancer
+resource "aws_security_group" "alb_security_group" {
+
+  name        = format("%s-%s", var.ecs_cluster_name, "ecs-alb-sg")
+  vpc_id      = data.aws_vpc.main.id
+  description = format("ECS %s ALB Security Group", var.ecs_cluster_name)
 }
 
 
 #
 # Inbound rules
 # Allow only ports are in variables ports_ingress & default_ports_ingress
-resource "aws_security_group_rule" "ecs_security_group_ingress" {
+resource "aws_security_group_rule" "alb_security_group_ingress" {
   for_each = toset(concat(var.ports_ingress, var.default_ports_ingress))
 
   from_port         = tonumber(each.key)
   to_port           = tonumber(each.key)
   protocol          = "tcp"
-  security_group_id = aws_security_group.ecs_security_group.id
+  security_group_id = aws_security_group.alb_security_group.id
   type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-
-#
-# Outbound rules
-# Allow all egress connections
-resource "aws_security_group_rule" "ecs_security_group_egress" {
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.ecs_security_group.id
-  type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
